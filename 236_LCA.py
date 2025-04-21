1539069c0c7659916eb9606ce3f7dda699a507f3
Rahimpanah <reyhaneh.rahimpanah@gmail.com>
2025-04-21 17:27:19 -0400

    Initial commit

---
+++ 236_LCA.py
@@ -0,0 +1,19 @@
+class Solution:
+    def lowestCommonAncestor(self, root, p, q):
+        stack=[root]
+        parent = {root: None}
+        while p not in parent or q not in parent:
+            node = stack.pop()
+            if node.left:
+                parent[node.left]=node
+                stack.append(node.left)
+            if node.right:
+                parent[node.right]=node
+                stack.append(node.right)
+        p_ancestors=[]
+        while p:
+            p_ancestors.append(p)
+            p = parent [p]
+        while q not in p_ancestors:
+            q = parent [q]
+        return q
